2149bd523f906885788a1f6892ddc6ff96d6641b
Margarita Krutikova <margaretkru@gmail.com>
2017-12-27 16:38:53 +0100

    Make one webpack config for build and hot reload, set up sass loader, add typography styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { AppContainer } from 'react-hot-loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import './styles/main.scss';
 
 const render = (Component) => {
   ReactDOM.render(
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,122 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const config = {
-  entry: {
-    vendor: [
+const buildWebpackConfig = (env) => {
+  // flags
+  const isHot = process.argv.indexOf('--hot') !== -1;
+  const isProduction = process.argv.indexOf('-p') !== -1;
+
+  const ExtractTextPluginConfig = new ExtractTextPlugin({
+    filename: 'site.css',
+    allChunks: true,
+    disable: isHot
+  });
+
+  const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './index.html',
+    filename: 'index.html',
+    inject: 'body'
+  });
+  const plugins = [ExtractTextPluginConfig, HtmlWebpackPluginConfig];
+
+  let sourceMaps = !isProduction;
+  let minimize = isProduction; 
+
+  // different entry configuration for hot reloading
+  const webpackEntry = {};
+
+  if (isHot || (env && env.hot)) {
+    webpackEntry.app = [
+      'babel-polyfill',
+      'prop-types',
+      'react-hot-loader/patch',
+      './src/index.js'
+    ];
+  } else {
+    webpackEntry.app = ['./src/index.js'];
+    webpackEntry.vendor = [
       'babel-polyfill',
       'react',
       'react-dom',
       'prop-types',
       'axios'
-    ],
-    app: ['./src/index.js']
-  },
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: '[name].js',
-    publicPath: ''
-  },
-  resolve: {
-    alias: {
-      'Services': path.resolve(__dirname, './src/services/')
+    ];
+    plugins.push(
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        minChunks: Infinity
+      }));
+    
+    if (isProduction) {
+      plugins.push(new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }));
     }
-  },
-  module: {
-    rules: [
-      { 
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        } 
+  }
+
+  return {
+    entry: webpackEntry,
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: '[name].js',
+      publicPath: '/'
+    },
+    plugins: plugins,
+    resolve: {
+      alias: {
+        'Services': path.resolve(__dirname, './src/services/')
       }
-    ]
-  },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: Infinity
-    })
-  ]
+    },
+    module: {
+      rules: [
+        { 
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          } 
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|svg)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'url-loader?limit=1024'
+          }
+        },
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [{
+              loader: 'css-loader',
+              options: {
+                modules: false,
+                sourceMap: sourceMaps,
+                minimize: minimize
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: sourceMaps,
+                plugins: () => 
+                  ([require('autoprefixer')({ browsers: 'last 5 versions' })])
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: sourceMaps,
+                minimize: minimize
+              }
+            }]
+          })
+        }
+      ]
+    }
+  };
 };
 
-module.exports = config;
+module.exports = buildWebpackConfig;
