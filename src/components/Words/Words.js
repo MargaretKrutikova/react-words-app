e9fb270eb86a1dbd16d99b0af8ad3d29a503a697
Margarita Krutikova <margarita.krutikova@starrepublic.com>
2018-06-10 21:31:46 +0200

    Add spinner on words page

--- src/components/Words/Words.js
+++ src/components/Words/Words.js
@@ -2,19 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Word from './Word';
 import PageHeader from '../Header/PageHeader';
+import SpinnerIcon from '../Icons/SpinnerIcon';
 import './_Words.scss';
 
 const Words = (props) => (
   <div className='words-list mb-4'>
     <PageHeader>Swedish words</PageHeader>
 
-    { props.loading && <h4>Loading...</h4> }
+    {props.loading &&
+      <React.Fragment>
+        <h4>Loading...</h4>
+        <SpinnerIcon className='spinner-icon' width={200} height={200} />
+      </ React.Fragment>
+    }
 
-    { !props.loading && props.words.map((word, index) => (
+    {!props.loading && props.words.map((word, index) => (
       <div key={index} className='d-flex justify-content-between py-3 words-list__item'>
         <Word word={word} />
         <Link to={`/edit/${word._id}`}>
-                Edit
+          Edit
         </Link>
       </div>
     ))}
