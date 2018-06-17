ba5ca34ec2d06bceba9e13fcc8cfc32bddfd0ba9
Margarita Krutikova <margarita.krutikova@starrepublic.com>
2018-06-17 18:13:00 +0200

    Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,14 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
     ecmaFeatures: {
+      "ecmaVersion": 6,
       experimentalObjectRestSpread: true,
-      jsx: true,
+      "jsx": true,
+      "modules": true,
+      "arrowFunctions": true,
+      "classes": true,
+      "object-shorthand": true,
+      "prefer-destructuring": true
     },
     sourceType: 'module',
   },
@@ -26,16 +32,69 @@ module.exports = {
     'no-console': ['warn', { allow: ['info', 'error'] }],
     'arrow-parens': ['error', 'always'],
     'linebreak-style': ["error", "windows"],
+    "no-unused-vars": 1,
+    "no-console": 0,
+    "semi": 0,
+    "quotes": [2, "single", { "allowTemplateLiterals": true }],
+    "flowtype/boolean-style": [
+      2,
+      "boolean"
+    ],
     "flowtype/define-flow-type": 1,
-    "flowtype/space-before-type-colon": [
-      1,
+    "flowtype/delimiter-dangle": [
+      2,
       "never"
     ],
-    "flowtype/use-flow-type": 1,
-    "flowtype/valid-syntax": 1,
+    "flowtype/generic-spacing": [
+      2,
+      "never"
+    ],
+    "flowtype/no-primitive-constructor-types": 2,
+    "flowtype/no-types-missing-file-annotation": 2,
+    "flowtype/no-weak-types": 2,
+    "flowtype/object-type-delimiter": [
+      2,
+      "comma"
+    ],
+    "flowtype/require-parameter-type": 2,
+    "flowtype/require-return-type": [
+      2,
+      "always",
+      {
+        "annotateUndefined": "never"
+      }
+    ],
+    "flowtype/require-valid-file-annotation": 2,
+    "flowtype/semi": [
+      2,
+      "always"
+    ],
+    "flowtype/space-after-type-colon": [
+      2,
+      "always"
+    ],
+    "flowtype/space-before-generic-bracket": [
+      2,
+      "never"
+    ],
+    "flowtype/space-before-type-colon": [
+      2,
+      "never"
+    ],
     "flowtype/type-id-match": [
       2,
-      "^([A-Z]+[a-z0-9A-Z]*)$"
-    ]
+      "^([A-Z][a-z0-9]+)+Type$"
+    ],
+    "flowtype/union-intersection-spacing": [
+      2,
+      "always"
+    ],
+    "flowtype/use-flow-type": 1,
+    "flowtype/valid-syntax": 1
   },
+  "settings": {
+    "flowtype": {
+      "onlyFilesWithFlowAnnotation": true
+    }
+  }
 };
